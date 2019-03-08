689b1805cebdbd2a409264b304ac6731c10b0b7d
Chris Mccluer <cmccluer@labconco.com>
2019-03-08 13:09:57 -0600

    Hide buttons and group box text

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -33,6 +33,7 @@ Partial Class Form1
         Me.ListBox2 = New System.Windows.Forms.ListBox()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button8 = New System.Windows.Forms.Button()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
@@ -40,7 +41,6 @@ Partial Class Form1
         Me.Label10 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button7 = New System.Windows.Forms.Button()
-        Me.Button8 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -48,26 +48,31 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(112, 19)
+        Me.Button1.Location = New System.Drawing.Point(149, 23)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 29)
+        Me.Button1.Size = New System.Drawing.Size(100, 36)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Get Links"
         Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(204, 19)
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(272, 23)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(467, 134)
+        Me.ListBox1.Size = New System.Drawing.Size(621, 164)
         Me.ListBox1.TabIndex = 1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(204, 165)
+        Me.Button2.Location = New System.Drawing.Point(272, 203)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(467, 26)
+        Me.Button2.Size = New System.Drawing.Size(623, 32)
         Me.Button2.TabIndex = 3
         Me.Button2.Text = "Select highlighted file to copy as new model file and attach to .dft file"
         Me.Button2.UseVisualStyleBackColor = True
@@ -75,25 +80,28 @@ Partial Class Form1
         'TextBox3
         '
         Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox3.Location = New System.Drawing.Point(142, 16)
+        Me.TextBox3.Location = New System.Drawing.Point(189, 20)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(116, 29)
+        Me.TextBox3.Size = New System.Drawing.Size(153, 34)
         Me.TextBox3.TabIndex = 4
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(159, 52)
+        Me.Label3.Location = New System.Drawing.Point(212, 64)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.Size = New System.Drawing.Size(62, 17)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "New P/N"
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(369, 42)
+        Me.Button4.Location = New System.Drawing.Point(492, 52)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(280, 23)
+        Me.Button4.Size = New System.Drawing.Size(373, 28)
         Me.Button4.TabIndex = 11
         Me.Button4.Text = "Accept Displayed Path and Create New .dft File"
         Me.Button4.UseVisualStyleBackColor = True
@@ -102,34 +110,40 @@ Partial Class Form1
         '
         Me.TextBox6.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.ModelLink.My.MySettings.Default, "LastPath", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
         Me.TextBox6.HideSelection = False
-        Me.TextBox6.Location = New System.Drawing.Point(369, 16)
+        Me.TextBox6.Location = New System.Drawing.Point(492, 20)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(278, 20)
+        Me.TextBox6.Size = New System.Drawing.Size(369, 22)
         Me.TextBox6.TabIndex = 12
         Me.TextBox6.Text = Global.ModelLink.My.MySettings.Default.LastPath
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(68, 19)
+        Me.Button5.Location = New System.Drawing.Point(91, 23)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(112, 23)
+        Me.Button5.Size = New System.Drawing.Size(149, 28)
         Me.Button5.TabIndex = 14
         Me.Button5.Text = "Call GetAsmInfo"
         Me.Button5.UseVisualStyleBackColor = True
+        Me.Button5.Visible = False
         '
         'ListBox2
         '
         Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.Location = New System.Drawing.Point(200, 16)
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(267, 20)
+        Me.ListBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(464, 82)
+        Me.ListBox2.Size = New System.Drawing.Size(617, 100)
         Me.ListBox2.TabIndex = 15
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(200, 104)
+        Me.Button6.Location = New System.Drawing.Point(267, 128)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(467, 26)
+        Me.Button6.Size = New System.Drawing.Size(623, 32)
         Me.Button6.TabIndex = 16
         Me.Button6.Text = "Save selected file as new model, attach to .asm, attach .asm to .dft"
         Me.Button6.UseVisualStyleBackColor = True
@@ -142,20 +156,32 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.Label8)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.TextBox3)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 6)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 7)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(759, 75)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(1012, 92)
         Me.GroupBox1.TabIndex = 17
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(871, 20)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(121, 28)
+        Me.Button8.TabIndex = 13
+        Me.Button8.Text = "Change Path"
+        Me.Button8.UseVisualStyleBackColor = True
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(18, 32)
+        Me.Label8.Location = New System.Drawing.Point(24, 39)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(80, 25)
+        Me.Label8.Size = New System.Drawing.Size(98, 31)
         Me.Label8.TabIndex = 7
         Me.Label8.Text = "Step 1"
         '
@@ -163,9 +189,10 @@ Partial Class Form1
         '
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.Location = New System.Drawing.Point(15, 92)
+        Me.Label9.Location = New System.Drawing.Point(20, 113)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(80, 25)
+        Me.Label9.Size = New System.Drawing.Size(98, 31)
         Me.Label9.TabIndex = 7
         Me.Label9.Text = "Step 2"
         '
@@ -175,12 +202,13 @@ Partial Class Form1
         Me.GroupBox2.Controls.Add(Me.Button2)
         Me.GroupBox2.Controls.Add(Me.ListBox1)
         Me.GroupBox2.Controls.Add(Me.Button1)
-        Me.GroupBox2.Location = New System.Drawing.Point(15, 87)
+        Me.GroupBox2.Location = New System.Drawing.Point(20, 107)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(755, 204)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(1007, 251)
         Me.GroupBox2.TabIndex = 18
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
         '
         'GroupBox3
         '
@@ -189,59 +217,55 @@ Partial Class Form1
         Me.GroupBox3.Controls.Add(Me.Button6)
         Me.GroupBox3.Controls.Add(Me.ListBox2)
         Me.GroupBox3.Controls.Add(Me.Button5)
-        Me.GroupBox3.Location = New System.Drawing.Point(19, 300)
+        Me.GroupBox3.Location = New System.Drawing.Point(25, 369)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(750, 171)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox3.Size = New System.Drawing.Size(1000, 210)
         Me.GroupBox3.TabIndex = 19
         Me.GroupBox3.TabStop = False
-        Me.GroupBox3.Text = "GroupBox3"
         '
         'Label10
         '
         Me.Label10.AutoSize = True
         Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label10.Location = New System.Drawing.Point(11, 73)
+        Me.Label10.Location = New System.Drawing.Point(15, 90)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(173, 25)
+        Me.Label10.Size = New System.Drawing.Size(213, 31)
         Me.Label10.TabIndex = 7
         Me.Label10.Text = "Step 3 (if .asm)"
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(200, 136)
+        Me.Button3.Location = New System.Drawing.Point(267, 167)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(467, 26)
+        Me.Button3.Size = New System.Drawing.Size(623, 32)
         Me.Button3.TabIndex = 16
         Me.Button3.Text = "Attach .asm to .dft with no changes to parts"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(19, 477)
+        Me.Button7.Location = New System.Drawing.Point(25, 587)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.Size = New System.Drawing.Size(100, 28)
         Me.Button7.TabIndex = 20
         Me.Button7.Text = "Quit"
         Me.Button7.UseVisualStyleBackColor = True
         '
-        'Button8
-        '
-        Me.Button8.Location = New System.Drawing.Point(653, 16)
-        Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(91, 23)
-        Me.Button8.TabIndex = 13
-        Me.Button8.Text = "Change Path"
-        Me.Button8.UseVisualStyleBackColor = True
-        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(795, 501)
+        Me.ClientSize = New System.Drawing.Size(1060, 617)
         Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "ModelLink"
         Me.GroupBox1.ResumeLayout(False)
